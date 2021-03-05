6ef39cdc63fed130e699fcff316fda744c352822
sana chowdhury:git config --global user.name sana <sana.chowdhury86@gmail.com>
2021-03-05 10:51:36 -0500

    modified the special characters required

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//special characters that can be generated into the password
+//the special characters that can be generated into the password
 let specialCharacters = ['+','#','?','!','%','*','^','&','@','~','.','/'];
 //upper case alphabets
 let upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 //lower case alphabets
 let lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-//numbers
+//numbers 1-9
 let numericCharacters = ['0','1','2','3','4','5','6','7','8','9']; 
 
 //functions for calling to get a password generated
