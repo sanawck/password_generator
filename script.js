866fa11fbdd7fb508f117dc94a06cc271b2b97de
sana chowdhury:git config --global user.name sana <sana.chowdhury86@gmail.com>
2021-03-04 21:51:16 -0500

    added confirmation prompts to js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -14,17 +14,17 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //special characters that can be generated into the password
-var specialCharacters = ['+','#','?','!','%','*','^','&','@','~','.','/'];
+let specialCharacters = ['+','#','?','!','%','*','^','&','@','~','.','/'];
 //upper case alphabets
-var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+let upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 //lower case alphabets
-var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 //numbers
-var numericCharacters = ['0','1','2','3','4','5','6','7','8','9']; 
+let numericCharacters = ['0','1','2','3','4','5','6','7','8','9']; 
 
 //functions for calling to get a password generated
 function generatePassword() {
-  var length = 8 ( // confused about this section?
+  let length = 8 ( // confused about this section?
     promt('How many characters would you like your password to be?')
   );
 //password must be 8 characters
@@ -34,7 +34,19 @@ if (length < 8){
 //password must be less than 129 characters
 if (length > 129) {
   alert('Password must be less than 128 characters')
-}
-
 
 }
+}
+//confirmation prompts for user to confirm 
+let hasLowerCasedCharacters = confirm (
+  'press ok to confirm adding lower case characters '
+);
+let hasNumericCharacters = confirm (
+  'press ok to confirm adding numeric characters'
+);
+let hasUpperCasedCharacters = confirm (
+  'press ok to confirm adding upper cased characters'
+);
+let hasSpecialCharacters = confirm(
+  'press ok to confirm adding special characters'
+);
